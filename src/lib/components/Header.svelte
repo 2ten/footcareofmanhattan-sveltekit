f147e76db43e6b6ab6a592546256b63f3902d96b
Michel Cohen <m@2ten.in>
2024-01-02 17:28:51 -0500

    Header updates

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,10 +1,14 @@
 <script>
 	import { PrismicLink, PrismicText } from '@prismicio/svelte';
+	import { browser } from '$app/environment';
+	import { page } from '$app/stores';
 
 	/** @type {import("@prismicio/client").Content.SettingsDocument} */
 	export let settings;
 	/** @type {import("@prismicio/client").Content.NavigationDocument} */
 	export let navigation;
+
+	/* peek-a-boo header */
 
 	let previousY
 	let currentY
@@ -20,7 +24,18 @@
 	$: scrollDirection = getDirection(currentY);
 	$: offscreen = scrollDirection === 'down' && currentY > clientHeight * 4
 
-	$: open = false
+
+	/* mobile nav */
+	let active = false;
+	$: $page.url && (active = false)
+	$: if(browser){
+		document.body.classList.toggle('overflow-hidden', active);
+	}
+
+	/* nav active class */
+	let navClass = 'md:py-0 font-semibold tracking-tight text-slate-800';
+	let navClassActive = 'md:py-0 font-semibold tracking-tight text-cyan-800';
+
 </script>
 <svelte:window bind:scrollY={currentY} />
 
@@ -33,12 +48,11 @@ bind:clientHeight
 			<a href="/" class="text-xl font-semibold tracking-tight">
 				{settings.data.site_title}
 			</a>
-			<nav class="fixed top-0 bottom-0 right-0 translate-x-full bg-white w-80 max-w-80 transition-transform ease-in md:w-auto md:static md:bg-transparent md:translate-x-0"
-			class:motion-safe:translate-x-0={open}
-			>
-				<ul class="p-8 md:p-0 md:flex flex-wrap gap-6 md:gap-10">
+			<nav class="md:w-auto md:static md:bg-transparent md:translate-x-0">
+				<ul class="hidden md:p-0 md:flex flex-wrap md:gap-10">
 					{#each navigation.data?.links as item}
-						<li class="py-2 md:py-0 font-semibold tracking-tight text-slate-800">
+						{@const activeLink = item.link.url === $page.url.pathname}
+						<li class={activeLink ? navClassActive : navClass}>
 							<PrismicLink field={item.link}>
 								<PrismicText field={item.label} />
 							</PrismicLink>
@@ -46,9 +60,26 @@ bind:clientHeight
 					{/each}
 				</ul>
 			</nav>
-			<button class="md:hidden z-50" on:click={() =>  open = !open}>
-				Menu
-			</button>
+			<button class="md:hidden z-50 w-12" 
+				aria-expanded={active ? "true" : "false"} 
+				aria-controls="mobileNav" 
+				aria-label={active ? "Close Navigation" : "Open Navigation"} 
+				class:active
+				on:click={() => active = !active} ><hr /><hr /><hr /></button>
 		</div>
 	</div>
 </header>
+{#if active}
+<nav class="fixed z-50 top-0 bottom-0 right-0 translate-x-full bg-white w-80 max-w-80 transition-transform ease-in md:hidden" id="mobileNav" class:motion-safe:translate-x-0={active}>
+	<ul class="p-8" role="navigation">
+		{#each navigation.data?.links as item}
+			{@const activeLink = item.link.url === $page.url.pathname}
+			<li class={activeLink ? navClassActive : navClass}>			
+				<PrismicLink field={item.link}>
+					<PrismicText field={item.label} />
+				</PrismicLink>
+			</li>
+		{/each}
+	</ul>
+</nav>
+{/if}
